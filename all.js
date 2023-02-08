a34435f00a9615f9739ba3742052328d0aeff0f8
Ramani Ganason <ramaniG@users.noreply.github.com>
2023-02-08 11:50:41 +0800

    Add files via upload

--- all.js
+++ all.js
@@ -1,17 +1,21 @@
 require([
     "esri/portal/Portal",
     "esri/identity/OAuthInfo",
-    "esri/identity/IdentityManager"
-], function (Portal, OAuthInfo, esriId) {
+    "esri/identity/IdentityManager",
+    "esri/config",
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/layers/FeatureLayer",
+    "esri/portal/PortalQueryParams"
+], function (Portal, OAuthInfo, esriId, esriConfig, Map, MapView, FeatureLayer, PortalQueryParams) {
     const clientId = "JiM7IJ9k519eIGIR";
-    const redirectUri = window.location.origin + "/authenticate.html";
-
+    const redirectUri = window.location.origin + "/oauth-callback.html";
 
     const info = new OAuthInfo({
         appId: clientId,
-        popup: false, // the default,
-        //flowType: "authorization-code",
-        //popupCallbackUrl: redirectUri
+        popup: true,
+        flowType: "authorization-code",
+        popupCallbackUrl: redirectUri
     });
 
     esriId.registerOAuthInfos([info]);
@@ -24,7 +28,6 @@ require([
 
         .catch(() => {
             handleSignedOut();
-
         });
 
     function handleSignedIn() {
@@ -33,6 +36,8 @@ require([
             const results = { name: portal.user.fullName, username: portal.user.username };
             document.getElementById("results").innerText = JSON.stringify(results, null, 2);
         });
+        displayPortalItems();
+        displayMapWithLayer();
     }
 
     function handleSignedOut() {
@@ -40,70 +45,76 @@ require([
     }
 
     document.getElementById("withPopupButton").addEventListener("click", function () {
-        esriId.getCredential(info.portalUrl + "/sharing");
+        // user will be redirected to OAuth sign-in page
+        esriId.getCredential((info.portalUrl + "/sharing"), {
+            oAuthPopupConfirmation: false
+        }).then(function () {
+            handleSignedIn();
+        });
     });
 
     document.getElementById("signOutButton").addEventListener("click", function () {
         esriId.destroyCredentials();
         window.location.reload();
     });
+
+    function displayMapWithLayer() {
+        const map = new Map({
+            basemap: "arcgis-topographic" // Basemap layer service
+        });
+
+        const view = new MapView({
+            map: map,
+            center: [122.642934, -25.404205], // Longitude, latitude
+            zoom: 13, // Zoom level
+            container: "viewDiv" // Div element
+        });
+
+        //Trailheads feature layer (points)
+        const WATenemantsLayer = new FeatureLayer({
+            url: "https://services.arcgis.com/1zcGoMGglHWsF36l/ArcGIS/rest/services/WA_Current_Tenements/FeatureServer/0",
+        });
+
+        map.add(WATenemantsLayer, 0);
+    }
+
+    function displayPortalItems() {
+        const portal = new Portal();
+        // Setting authMode to immediate signs the user in once loaded
+        portal.authMode = "immediate";
+        // Once loaded, user is signed in
+        portal.load().then(() => {
+            // Create query parameters for the portal search
+            const queryParams = new PortalQueryParams({
+                query: "owner:" + portal.user.username,
+                sortField: "numViews",
+                sortOrder: "desc",
+                num: 20
+            });
+
+            // Query the items based on the queryParams created from portal above
+            portal.queryItems(queryParams).then(createGallery);
+        });
+    }
+
+    function createGallery(items) {
+        let htmlFragment = "";
+
+        items.results.forEach((item) => {
+            htmlFragment +=
+                '<div class="esri-item-container">' +
+                (item.thumbnailUrl
+                    ? '<div class="esri-image" style="background-image:url(' + item.thumbnailUrl + ');"></div>'
+                    : '<div class="esri-image esri-null-image">Thumbnail not available</div>') +
+                (item.title
+                    ? '<div class="esri-title">' + (item.title || "") + "</div>"
+                    : '<div class="esri-title esri-null-title">Title not available</div>') +
+                ('<div class="esri-title">Access : ' + (item.access || "") + "</div>") +
+                ('<div class="esri-title">Feature Layer : ' + (item.isLayer) + "</div>") +
+                ('<div class="esri-title">Type : ' + (item.type || "") + "</div>") +
+                ('<div class="esri-title">URL : ' + (item.url || "") + "</div>") +
+                "</div>";
+        });
+        document.getElementById("listitems").innerHTML = htmlFragment;
+    }
 });
-
-
-// import { ArcGISIdentityManager } from 'https://cdn.skypack.dev/@esri/arcgis-rest-request@4.0.0';
-
-// let session = null;
-// const clientId = "JiM7IJ9k519eIGIR";
-// const redirectUri = window.location.origin + "/authenticate.html";
-
-// const serializedSession = localStorage.getItem("__ARCGIS_REST_USER_SESSION__"); // Check to see if there is a serialized session in local storage.
-
-// if (serializedSession !== null && serializedSession !== "undefined") {
-//     session = ArcGISIdentityManager.deserialize(serializedSession);
-// }
-
-// function updateSessionInfo(session) {
-//     let sessionInfo = document.getElementById("sessionInfo");
-
-//     if (session) {
-//         sessionInfo.innerHTML = "Logged in as " + session.username;
-//         localStorage.setItem("__ARCGIS_REST_USER_SESSION__", session.serialize());
-//         document.getElementById("withPopupButton").hidden = true;
-//         document.getElementById("signOutButton").hidden = false;
-//     } else {
-//         sessionInfo.innerHTML = "";
-//         document.getElementById("withPopupButton").hidden = false;
-//         document.getElementById("signOutButton").hidden = true;
-//     }
-// }
-
-// updateSessionInfo(session);
-
-// document.getElementById("withPopupButton").addEventListener("click", (event) => {
-//     // Begin an OAuth2 login using a popup.
-//     ArcGISIdentityManager.beginOAuth2({
-//         clientId: clientId,
-//         redirectUri: redirectUri,
-//         popup: true
-//     })
-//         .then((newSession) => {
-//             // Upon a successful login, update the session with the new session.
-//             session = newSession;
-//             console.log(session);
-//             updateSessionInfo(session);
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//         });
-//     event.preventDefault();
-// });
-
-// document.getElementById("signOutButton").addEventListener("click", (event) => {
-//     event.preventDefault();
-//     // call the signOut method to invalidate the token.
-//     session.signOut().then(() => {
-//         session = null; // Clear the session from memory.
-//         localStorage.removeItem("__ARCGIS_REST_USER_SESSION__");
-//         updateSessionInfo();
-//     });
-// });
